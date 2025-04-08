e42d918be68fe1916584f16a19026881a72e0d3c
Helder Almeida <helder@plathion.com>
2025-04-08 15:36:22 +0000

    Removido pastas pelo gitIgnore

--- node/app.js
+++ node/app.js
@@ -6,13 +6,19 @@ const port = 3000;
 
 app.get("/", async (req, res) => {
   try {
-    var sql = "INSERT INTO people (name) VALUES ('Helder')";
+    var sql =
+      "Create table if not exists people (id int auto_increment primary key, name varchar(255))";
+    await executarSQL(sql);
+    console.log("Tabela people criada com sucesso");
+
+    var sql = "Insert Into people (name) Values ('Helder')";
     await executarSQL(sql);
     console.log("Registro inserido com sucesso");
 
-    sql = "SELECT name FROM people";
+    sql = "Select name From people";
     const resultados = await executarSQL(sql);
     console.log(resultados);
+
     res.send("<h1>Full Cycle Rocks!</h1>" + JSON.stringify(resultados));
   } catch (error) {
     console.error("Erro ao executar a consulta:", error);
